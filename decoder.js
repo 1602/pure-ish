ecf936e2ef1c06d1835aacceed56a13c901679ec
Anatoliy <mail@anatoliy.in>
2018-03-03 15:35:39 +0000

    Added decoders

---
+++ decoder.js
@@ -0,0 +1,342 @@
+'use strict';
+
+module.exports = {
+    decodeValue,
+    value,
+    string,
+    number,
+    boolean,
+    object,
+    array,
+    constant,
+    oneOf,
+    optional,
+    nullable,
+};
+
+function decodeValue(decoder, v) {
+    if (decoder.type === 'value') {
+        return {
+            result: 'success',
+            data: v,
+        };
+    }
+
+    if (decoder.type === 'string') {
+        return decodeString(v);
+    }
+
+    if (decoder.type === 'number') {
+        return decodeNumber(v);
+    }
+
+    if (decoder.type === 'boolean') {
+        return decodeBoolean(v);
+    }
+
+    if (decoder.type === 'object') {
+        return decodeObject(decoder.spec, v);
+    }
+
+    if (decoder.type === 'array') {
+        return decodeArray(decoder.spec, v);
+    }
+
+    if (decoder.type === 'constant') {
+        return decodeConstant(decoder.spec, v);
+    }
+
+    if (decoder.type === 'oneOf') {
+        return decodeOneOf(decoder.spec, v);
+    }
+}
+
+function decodeString(value) {
+    if (value === null) {
+        return {
+            result: 'fail',
+            error: `Expected string but received null`,
+        };
+    }
+
+    const actualType = typeof value;
+
+    if (actualType === 'undefined') {
+        return {
+            result: 'fail',
+            error: `Expected string but received undefined`,
+        };
+    }
+
+    if (actualType === 'string') {
+        return {
+            result: 'success',
+            data: value,
+        };
+    }
+
+    return {
+        result: 'fail',
+        error: `Expected string but received ${ actualType } ${ value }`,
+    };
+}
+
+function decodeNumber(value) {
+    if (value === null) {
+        return {
+            result: 'fail',
+            error: `Expected number but received null`,
+        };
+    }
+
+    const actualType = typeof value;
+
+    if (actualType === 'undefined') {
+        return {
+            result: 'fail',
+            error: `Expected number but received undefined`,
+        };
+    }
+
+    if (actualType === 'number') {
+        return {
+            result: 'success',
+            data: value,
+        };
+    }
+
+    return {
+        result: 'fail',
+        error: `Expected number but received ${ actualType } ${ value }`,
+    };
+}
+
+function decodeBoolean(value) {
+    if (value === null) {
+        return {
+            result: 'fail',
+            error: `Expected boolean but received null`,
+        };
+    }
+
+    const actualType = typeof value;
+
+    if (actualType === 'undefined') {
+        return {
+            result: 'fail',
+            error: `Expected boolean but received undefined`,
+        };
+    }
+
+    if (actualType === 'boolean') {
+        return {
+            result: 'success',
+            data: value,
+        };
+    }
+
+    return {
+        result: 'fail',
+        error: `Expected boolean but received ${ actualType } ${ value }`,
+    };
+}
+
+function decodeObject(spec, value) {
+    if (value === null) {
+        return {
+            result: 'fail',
+            error: `Expected object but received null`,
+        };
+    }
+
+    const actualType = typeof value;
+
+    if (actualType === 'undefined') {
+        return {
+            result: 'fail',
+            error: `Expected object but received undefined`,
+        };
+    }
+
+    if (actualType === 'object') {
+        return Object
+            .keys(spec)
+            .reduce((accumulator, specKey) => {
+                if (accumulator.result === 'fail') {
+                    return accumulator;
+                }
+
+                const propertyDecoder = spec[specKey];
+                const decodingPropertyResult = decodeValue(propertyDecoder, value[specKey]);
+                if (decodingPropertyResult.result === 'success') {
+                    accumulator.data[specKey] = decodingPropertyResult.data;
+                    return accumulator;
+                }
+
+                decodingPropertyResult.error = specKey + ': ' + decodingPropertyResult.error;
+
+                return decodingPropertyResult;
+            }, { result: 'success', data: {} });
+    }
+
+    return {
+        result: 'fail',
+        error: `Expected object but received ${ actualType } ${ value }`,
+    };
+}
+
+function decodeArray(decoder, value) {
+    if (value === null) {
+        return {
+            result: 'fail',
+            error: `Expected array but received null`,
+        };
+    }
+
+    const actualType = typeof value;
+
+    if (actualType === 'undefined') {
+        return {
+            result: 'fail',
+            error: `Expected array but received undefined`,
+        };
+    }
+
+    if (actualType !== 'object' || !Array.isArray(value)) {
+        return {
+            result: 'fail',
+            error: `Expected array but received ${ actualType }`,
+        };
+    }
+
+    return value.reduce((accumulator, value, index) => {
+        if (accumulator.result === 'fail') {
+            return accumulator;
+        }
+
+        const itemDecodingResult = decodeValue(decoder, value);
+        if (itemDecodingResult.result === 'success') {
+            accumulator.data.push(itemDecodingResult.data);
+            return accumulator;
+        }
+
+        itemDecodingResult.error = `Array item ${ index }: ${ itemDecodingResult.error }`;
+        return itemDecodingResult;
+    }, { result: 'success', data: [] });
+}
+
+function decodeConstant(spec, value) {
+    const actualType = typeof value;
+    const expectedType = typeof spec;
+
+    if (value === null && spec !== null) {
+        return {
+            result: 'fail',
+            error: `Expected ${ expectedType } ${ spec } but received null`,
+        };
+    }
+
+    if (actualType === 'undefined' && expectedType === 'undefined') {
+        return {
+            result: 'success',
+            data: value,
+        };
+    }
+
+    if (actualType === expectedType && spec === value) {
+        return {
+            result: 'success',
+            data: value,
+        };
+    }
+
+    return {
+        result: 'fail',
+        error: `Expected ${ expectedType } ${ spec } but received ${ actualType } ${ value }`,
+    };
+}
+
+function decodeOneOf(spec, value) {
+    return spec.reduce((accumulator, decoder) => {
+        if (accumulator.result === 'success') {
+            return accumulator;
+        };
+
+        const decodingResult = decodeValue(decoder, value);
+        if (decodingResult.result === 'fail') {
+            accumulator.error += '\n - ' + decodingResult.error;
+            return accumulator;
+        }
+
+        return decodingResult;
+    }, { result: 'fail', error: 'All decoders failed:' });
+}
+
+function value() {
+    return {
+        object: 'decoder',
+        type: 'value',
+    };
+}
+
+function string() {
+    return {
+        object: 'decoder',
+        type: 'string',
+    };
+}
+
+function boolean() {
+    return {
+        object: 'decoder',
+        type: 'boolean',
+    };
+}
+
+function number() {
+    return {
+        object: 'decoder',
+        type: 'number',
+    };
+}
+
+function object(spec) {
+    return {
+        object: 'decoder',
+        type: 'object',
+        spec,
+    };
+}
+
+function array(spec) {
+    return {
+        object: 'decoder',
+        type: 'array',
+        spec,
+    };
+}
+
+function constant(spec) {
+    return {
+        object: 'decoder',
+        type: 'constant',
+        spec,
+    };
+}
+
+function oneOf(spec) {
+    return {
+        object: 'decoder',
+        type: 'oneOf',
+        spec,
+    };
+}
+
+function optional(decoder) {
+    return oneOf([constant(), decoder]);
+}
+
+function nullable(decoder) {
+    return oneOf([constant(null), decoder]);
+}
+
---
+++ test/decoder.js
@@ -0,0 +1,234 @@
+'use strict';
+
+const expect = require('expect');
+const { decodeValue, value, string, number, boolean, array, object, constant, oneOf, optional, nullable } = require('../decoder.js');
+
+describe('Decoder', () => {
+
+    describe('value', () => {
+
+        it('always decodes successfully', () =>
+            expect(decodeValue(value(), 'hello')).toEqual({
+                result: 'success',
+                data: 'hello',
+            }));
+
+    });
+
+    describe('string', () => {
+
+        it('decodes successfully', () =>
+            expect(decodeValue(string(), 'hello')).toEqual({
+                result: 'success',
+                data: 'hello',
+            }));
+
+        it('decodes with error', () =>
+            expect(decodeValue(string(), 1602)).toEqual({
+                result: 'fail',
+                error: 'Expected string but received number 1602',
+            }));
+
+        it('expects non-null value', () =>
+            expect(decodeValue(string(), null))
+                .toEqual({
+                    result: 'fail',
+                    error: 'Expected string but received null',
+                }));
+
+        it('expect non-undefined value', () =>
+            expect(decodeValue(string(), undefined))
+                .toEqual({
+                    result: 'fail',
+                    error: 'Expected string but received undefined',
+                }));
+
+    });
+
+    describe('number', () => {
+
+        it('decodes successfully', () =>
+            expect(decodeValue(number(), 1602)).toEqual({
+                result: 'success',
+                data: 1602,
+            }));
+
+        it('decodes with error', () =>
+            expect(decodeValue(number(), '1602')).toEqual({
+                result: 'fail',
+                error: 'Expected number but received string 1602',
+            }));
+
+        it('expects non-null value', () =>
+            expect(decodeValue(number(), null))
+                .toEqual({
+                    result: 'fail',
+                    error: 'Expected number but received null',
+                }));
+
+        it('expect non-undefined value', () =>
+            expect(decodeValue(number(), undefined))
+                .toEqual({
+                    result: 'fail',
+                    error: 'Expected number but received undefined',
+                }));
+
+    });
+
+    describe('boolean', () => {
+
+        it('decodes successfully', () =>
+            expect(decodeValue(boolean(), true)).toEqual({
+                result: 'success',
+                data: true,
+            }));
+
+        it('decodes with error', () =>
+            expect(decodeValue(boolean(), '1602')).toEqual({
+                result: 'fail',
+                error: 'Expected boolean but received string 1602',
+            }));
+
+        it('expects non-null value', () =>
+            expect(decodeValue(boolean(), null))
+                .toEqual({
+                    result: 'fail',
+                    error: 'Expected boolean but received null',
+                }));
+
+        it('expect non-undefined value', () =>
+            expect(decodeValue(boolean(), undefined))
+                .toEqual({
+                    result: 'fail',
+                    error: 'Expected boolean but received undefined',
+                }));
+
+    });
+
+    describe('object', () => {
+        it('decodes successfully', () =>
+            expect(decodeValue(object({ foo: string() }), { foo: 'bar' }))
+                .toEqual({
+                    result: 'success',
+                    data: { foo: 'bar' },
+                }));
+
+        it('drops extra props', () =>
+            expect(decodeValue(object({ foo: string() }), { foo: 'bar', hey: 'jude' }))
+                .toEqual({
+                    result: 'success',
+                    data: { foo: 'bar' },
+                }));
+
+        it('expects non-null value', () =>
+            expect(decodeValue(object({}), null))
+                .toEqual({
+                    result: 'fail',
+                    error: 'Expected object but received null',
+                }));
+
+        it('expect non-undefined value', () =>
+            expect(decodeValue(object({}), undefined))
+                .toEqual({
+                    result: 'fail',
+                    error: 'Expected object but received undefined',
+                }));
+
+        it('fails when property decoding fails', () =>
+            expect(decodeValue(object({ foo: string() }), { foo: 1 }))
+                .toEqual({
+                    result: 'fail',
+                    error: 'foo: Expected string but received number 1',
+                }));
+    });
+
+    describe('array', () => {
+
+        it('decodes successfully', () =>
+            expect(decodeValue(array(string()), ['hello']))
+                .toEqual({
+                    result: 'success',
+                    data: ['hello'],
+                }));
+
+        it('decodes empty array successfully', () =>
+            expect(decodeValue(array(string()), []))
+                .toEqual({
+                    result: 'success',
+                    data: [],
+                }));
+
+        it('finds incorrect item', () =>
+            expect(decodeValue(array(string()), ['hello', 1, '2']))
+                .toEqual({
+                    result: 'fail',
+                    error: 'Array item 1: Expected string but received number 1',
+                }));
+
+    });
+
+
+    describe('constant', () => {
+
+        it('decodes successfully', () =>
+            expect(decodeValue(constant('exactly this'), 'exactly this')).toEqual({
+                result: 'success',
+                data: 'exactly this',
+            }));
+
+        it('requires type matching', () =>
+            expect(decodeValue(constant('1602'), 1602)).toEqual({
+                result: 'fail',
+                error: 'Expected string 1602 but received number 1602',
+            }));
+
+        it('expects non-null value', () =>
+            expect(decodeValue(constant('1602'), null))
+                .toEqual({
+                    result: 'fail',
+                    error: 'Expected string 1602 but received null',
+                }));
+
+        it('expect undefined value', () =>
+            expect(decodeValue(constant(), undefined))
+                .toEqual({
+                    result: 'success',
+                }));
+
+    });
+
+    describe('oneOf', () => {
+
+        it('decodes successfully', () =>
+            expect(decodeValue(oneOf([constant('exactly this'), constant(null)]), null)).toEqual({
+                result: 'success',
+                data: null,
+            }));
+
+        it('fails when all decoders fail', () =>
+            expect(decodeValue(oneOf([string(), number()]), null)).toEqual({
+                result: 'fail',
+                error: 'All decoders failed:\n - Expected string but received null\n - Expected number but received null',
+            }));
+
+    });
+
+    describe('optional', () => {
+
+        it('decodes successfully', () =>
+            expect(decodeValue(object({ hello: optional(string()) }), {})).toEqual({
+                result: 'success',
+                data: {},
+            }));
+    });
+
+    describe('nullable', () => {
+
+        it('decodes successfully', () =>
+            expect(decodeValue(object({ hello: nullable(string()) }), { hello: null })).toEqual({
+                result: 'success',
+                data: { hello: null },
+            }));
+    });
+
+});
